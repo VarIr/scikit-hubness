54e7b0b801a5647c21a8f795d0957b34077877a5
Roman Feldbauer <sci@feldbauer.org>
2019-06-27 08:29:59 +0200

    MAINT bump version

--- hubness/__init__.py
+++ hubness/__init__.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
 
-""" Python hubness package for nearest neighbor retrieval in high-dimensional space.
+""" Python hubness package for nearest neighbor retrieval in high-dimensional space."""
 
-This file is part of the HUBNESS package available at
-https://github.com/OFAI/hubness/
-The HUBNESS package is licensed under the terms of the GNU GPLv3.
-
-(c) 2018-2019, Roman Feldbauer
-Austrian Research Institute for Artificial Intelligence (OFAI) and
-University of Vienna, Division of Computational Systems Biology (CUBE)
-Contact: <roman.feldbauer@univie.ac.at>
-"""
-
-__version__ = '0.1.201906a2'
+__version__ = '0.1.201906a3'
 
 from . import analysis
 from .analysis.estimation import Hubness
