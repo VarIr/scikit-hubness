116b0df7269d5490ca22259769998503860ac33c
Roman Feldbauer <sci@feldbauer.org>
2019-06-28 15:22:12 +0200

    FEAT LSH radius query

--- hubness/neighbors/lsh.py
+++ hubness/neighbors/lsh.py
@@ -8,7 +8,7 @@ import warnings
 import numpy as np
 from sklearn.metrics import euclidean_distances
 from sklearn.metrics.pairwise import cosine_distances
-from sklearn.utils.validation import check_is_fitted
+from sklearn.utils.validation import check_is_fitted, check_array
 import falconn
 from tqdm.autonotebook import tqdm
 
@@ -22,10 +22,11 @@ class LSH(ApproximateNearestNeighbor):
     valid_metrics = ['SEuclideanDistance', 'sqeuclidean', 'minkowski',
                      'cosine', 'neg_inner', 'NegativeInnerProduct']
 
-    def __init__(self, n_candidates: int = 5, metric: str = 'sqeuclidean', num_probes: int = 50,
+    def __init__(self, n_candidates: int = 5, radius: float = 1., metric: str = 'sqeuclidean', num_probes: int = 50,
                  n_jobs: int = 1, verbose: int = 0):
         super().__init__(n_candidates=n_candidates, metric=metric, n_jobs=n_jobs, verbose=verbose)
         self.num_probes = num_probes
+        self.radius = radius
 
     def fit(self, X: np.ndarray, y: np.ndarray = None) -> LSH:
         """ Setup the LSH index from training data. """
@@ -52,7 +53,7 @@ class LSH(ApproximateNearestNeighbor):
 
         return self
 
-    def kneighbors(self, X: np.ndarray, n_candidates: int = None, return_distance: bool = True):
+    def kneighbors(self, X: np.ndarray = None, n_candidates: int = None, return_distance: bool = True):
         check_is_fitted(self, ["index_", 'X_train_'])
 
         # Check the n_neighbors parameter
@@ -64,7 +65,18 @@ class LSH(ApproximateNearestNeighbor):
             if not np.issubdtype(type(n_candidates), np.integer):
                 raise TypeError(f"n_neighbors does not take {type(n_candidates)} value, enter integer value")
 
-        # Constructing a query object
+        if X is not None:
+            query_is_train = False
+            X = check_array(X, accept_sparse='csr')
+            n_retrieve = n_candidates
+        else:
+            query_is_train = True
+            X = self.X_train_
+            # Include an extra neighbor to account for the sample itself being
+            # returned, which is removed later
+            n_retrieve = n_candidates + 1
+
+        # Configure the LSH query object
         query = self.index_.construct_query_object()
         query.set_num_probes(self.num_probes)
 
@@ -90,8 +102,9 @@ class LSH(ApproximateNearestNeighbor):
                                desc='LSH',
                                total=X.shape[0], )
         for i, x in enumerate_X:
-            # LSH will find object itself as 1-NN, but we remove it here
-            knn = np.array(query.find_k_nearest_neighbors(x, k=n_candidates + 1))[1:]
+            knn = np.array(query.find_k_nearest_neighbors(x, k=n_retrieve))
+            if query_is_train:
+                knn = knn[1:]
             neigh_ind[i, :knn.size] = knn
 
             if return_distance:
@@ -104,7 +117,66 @@ class LSH(ApproximateNearestNeighbor):
                 if return_distance:
                     neigh_dist[i, knn.size:] = np.nan
 
-        self.queryable_ = query
+        if return_distance:
+            return neigh_dist, neigh_ind
+        else:
+            return neigh_ind
+
+    def radius_neighbors(self, X: np.ndarray = None, radius: float = None, return_distance: bool = True):
+        """ TODO add docstring
+
+        Notes
+        -----
+        From the falconn docs: radius can be negative, and for the distance function
+        'negative_inner_product' it actually makes sense.
+        """
+        check_is_fitted(self, ["index_", 'X_train_'])
+
+        # Constructing a query object
+        query = self.index_.construct_query_object()
+        query.set_num_probes(self.num_probes)
+
+        if return_distance:
+            if self.metric == 'sqeuclidean':
+                distances = partial(euclidean_distances, squared=True)
+            elif self.metric == 'cosine':
+                distances = cosine_distances
+            else:
+                raise ValueError(f'Internal error: unrecognized metric "{self.metric}"')
+
+        if X is not None:
+            query_is_train = False
+            X = check_array(X, accept_sparse='csr')
+        else:
+            query_is_train = True
+            X = self.X_train_
+
+        if radius is None:
+            radius = self.radius
+        if self.metric == 'sqeuclidean':
+            radius *= radius
+
+        # Allocate memory for neighbor indices (and distances)
+        n_objects = X.shape[0]
+        neigh_ind = np.empty(n_objects, dtype='object')
+        if return_distance:
+            neigh_dist = np.empty_like(neigh_ind)
+
+        # If verbose, show progress bar on the search loop
+        if not self.verbose:
+            enumerate_X = enumerate(X)
+        else:
+            enumerate_X = tqdm(enumerate(X),
+                               desc='LSH',
+                               total=X.shape[0], )
+        for i, x in enumerate_X:
+            knn = np.array(query.find_near_neighbors(x, threshold=radius))
+            if query_is_train:
+                knn = knn[1:]
+            neigh_ind[i] = knn
+
+            if return_distance:
+                neigh_dist[i] = distances(x.reshape(1, -1), self.X_train_[knn])
 
         if return_distance:
             return neigh_dist, neigh_ind
