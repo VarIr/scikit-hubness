e858cef05bb921838cb41d98eae32cc1747aaef4
Roman Feldbauer <sci@feldbauer.org>
2019-07-05 09:45:46 +0200

    FIX travis on Linux

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -3,7 +3,7 @@
 set -e
 
 # the miniconda directory may exist if it has been restored from cache
-if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
+if [[ -d "$MINICONDA_DIR" ]] && [[ -e "$MINICONDA_DIR/bin/conda" ]]; then
     echo "Miniconda install already present from cache: $MINICONDA_DIR"
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
@@ -20,10 +20,14 @@ else # if it does not exist, we need to install miniconda
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
     export PATH="$PATH:$MINICONDA_DIR/bin"
+    echo $PATH
+    which pip
+    export PATH="$MINICONDA_DIR/bin:$PATH"
+    echo $PATH
+    which pip
+
     hash -r
     conda config --set always_yes yes --set changeps1 no
-    conda config --add channels bioconda
-    conda config --add channels r
     conda update -q conda
     conda info -a # for debugging
 fi
