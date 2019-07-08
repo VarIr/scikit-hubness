4487d79cf442ad515ca67b3a6cc1029d33ead465
Roman Feldbauer <sci@feldbauer.org>
2019-07-08 08:28:20 +0200

    FEAT experimental OSX support (#4)
    MacOS X support should work out of the box.
    Added osx to travis CI, and classification in setup.py

--- travis/install-conda.sh
+++ travis/install-conda.sh
@@ -3,24 +3,27 @@
 set -e
 
 # the miniconda directory may exist if it has been restored from cache
-if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ]; then
+if [[ -d "$MINICONDA_DIR" ]] && [[ -e "$MINICONDA_DIR/bin/conda" ]]; then
     echo "Miniconda install already present from cache: $MINICONDA_DIR"
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
 
-    if [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
+    if [[ "$TRAVIS_OS_NAME" == 'linux' ]] && [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
+        echo "WARNING: using Python2"
         wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-    else
+    elif [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
     fi
 
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
-    export PATH="$PATH:$MINICONDA_DIR/bin"
-    hash -r
+    export PATH="$MINICONDA_DIR/bin:$PATH"
     conda config --set always_yes yes --set changeps1 no
-    conda config --add channels bioconda
-    conda config --add channels r
     conda update -q conda
     conda info -a # for debugging
+    echo $TRAVIS_PYTHON_VERSION
+    conda create --yes -n test python=$TRAVIS_PYTHON_VERSION
+    source activate test
 fi
