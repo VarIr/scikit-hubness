102179f5a360fde94487384f5023ec7d27f22f1d
Roman Feldbauer <sci@feldbauer.org>
2019-09-03 11:39:07 +0200

    do use homebrew on MacOS, but check for installed packages

--- travis/install-build.sh
+++ travis/install-build.sh
@@ -8,9 +8,17 @@ if [[ $(uname) == "Darwin" ]]; then
   echo "Running under Mac OS X"
 
   # Setup environment
-  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  # brew install cmake
-  # brew install gcc@9
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if brew ls --versions cmake > /dev/null; then
+    echo "cmake already installed"
+  else
+    brew install cmake
+  fi
+  if brew ls --versions gcc@9 > /dev/null; then
+    echo "gcc@9 already installed"
+  else
+    brew install gcc@9
+  fi
   ln -s ./gcc-9 /usr/local/bin/gcc
   ln -s ./g++-9 /usr/local/bin/g++
   export CXX=g++
@@ -42,8 +50,10 @@ elif [[ $(uname -s) == Linux* ]]; then
 
   # Find the latest release
   FILE=$(curl -s https://api.github.com/repos/yahoojapan/NGT/releases/latest | grep zipball_url | cut -d '"' -f 4)
+  echo "Downloading $FILE"
   wget "$FILE"
   BNAME=$(basename "$FILE")
+  echo "Release is $BNAME"
 
   # Install NGT
   rm -rf yahoojapan-NGT-*
