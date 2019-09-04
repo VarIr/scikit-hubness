c02fdf6eb5f5c04581387acd15f00e469c2865ef
Roman Feldbauer <sci@feldbauer.org>
2019-09-04 10:54:48 +0200

    FIX mock annoy and ngt for sphinx [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 import mock
 MOCK_MODULES = ['falconn', 'nmslib',
+                'annoy', 'ngt',
                 ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
