beba675aed2d5efcc7ec2cf77f7672460a41c08a
Roman Feldbauer <sci@feldbauer.org>
2021-06-14 17:23:37 +0200

    FEAT DisSimLocal in sparse kneighbors graph

--- skhubness/reduction/dis_sim.py
+++ skhubness/reduction/dis_sim.py
@@ -5,11 +5,196 @@ from __future__ import annotations
 import warnings
 
 import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.base import TransformerMixin
 from sklearn.metrics import euclidean_distances
 from sklearn.utils.extmath import row_norms
 from sklearn.utils.validation import check_is_fitted, check_consistent_length, check_array
 
-from .base import HubnessReduction
+from .base import GraphHubnessReduction, HubnessReduction
+from ..utils.helper import k_neighbors_graph
+
+
+class GraphDisSimLocal(GraphHubnessReduction, TransformerMixin):
+    """ Hubness reduction with DisSimLocal [1]_ in an sklearn-compatible kneighbors_graph.
+
+    Parameters
+    ----------
+    k: int, default = 5
+        Number of neighbors to consider for the local centroids
+    return_squared_distances: bool, default = True
+        DisSimLocal operates on squared Euclidean distances.
+        If True, also return (quasi) squared Euclidean distances;
+        if False, return (quasi) Euclidean distances instead.
+    precompute_X_norm_squared : bool, default = True
+        Precomputing parts of the squared Euclidean distances saves computation while querying
+        at the cost higher memory consumption.
+
+    References
+    ----------
+    .. [1] Hara K, Suzuki I, Kobayashi K, Fukumizu K, Radovanović M (2016)
+           Flattening the density gradient for eliminating spatial centrality to reduce hubness.
+           In: Proceedings of the 30th AAAI conference on artificial intelligence, pp 1659–1665.
+           https://www.aaai.org/ocs/index.php/AAAI/AAAI16/paper/viewPaper/12055
+    """
+    def __init__(
+            self,
+            k: int = 5,
+            return_squared_distances: bool = True,
+            precompute_X_norm_squared: bool = True,
+            *args,
+            **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.k = k
+        self.return_squared = return_squared_distances
+        self.precompute_X_norm_squared = precompute_X_norm_squared
+
+    def fit(self, X: csr_matrix, y, **kwargs) -> GraphDisSimLocal:
+        """ Extract DisSimLocal parameters.
+
+        Parameters
+        ----------
+        X : sparse matrix of shape (n_indexed, n_indexed)
+            Squared Euclidean distances in sorted sparse neighbors graph,
+            such as obtained from sklearn.neighbors.kneighbors_graph.
+            Each row i must contain the neighbors of X_i in order of increasing distances,
+            that is, nearest neighbor first.
+        y : ignored
+        vectors : array-like of shape (n_indexed, n_features)
+            Indexed objects in vector representation.
+            DisSimLocal is formulated specifically for Euclidean distances, and requires access to the objects
+            in the original vector space. (In contrast to other hubness reduction methods, like mutual proximity
+            or local scaling, that operate on dissimilarity data).
+
+        Returns
+        -------
+        self
+
+        Notes
+        -----
+        Ensure sorting when using custom (approximate) neighbors implementations.
+        DisSimLocal strictly requires squared Euclidean distances, and returns undefined values otherwise.
+        """
+        X = check_array(X, accept_sparse=True)  # noqa
+        # check_kneighbors_graph(kng)  # TODO
+        X: csr_matrix = X.tocsr()
+
+        vectors_indexed = kwargs.get("vectors", None)
+        vectors_indexed: np.ndarray = check_array(vectors_indexed)  # noqa
+        # check X vs vectors TODO
+
+        n_indexed = X.shape[0]
+        n_neighbors = X.indptr[1]
+        try:
+            if self.k <= 0:
+                raise ValueError(f"Expected k > 0. Got {self.k}")
+        except TypeError:  # Why?
+            raise TypeError(f'Expected k: int > 0. Got {self.k}')
+        k = self.k
+
+        if k > n_neighbors.shape[1]:
+            k = n_neighbors
+            warnings.warn(f'Neighborhood parameter k larger than number of provided neighbors in X. Reducing to k={k}.')
+
+        # Calculate local neighborhood centroids among the training points
+        ind_knn = X.indices.reshape(n_indexed, -1)[:, :k]
+        centroids_indexed = vectors_indexed[ind_knn].mean(axis=1)
+        dist_to_cent = row_norms(vectors_indexed - centroids_indexed, squared=True)
+
+        self.vectors_indexed_ = vectors_indexed
+        self.centroids_indexed_ = centroids_indexed
+        self.dist_to_centroids_indexed_ = dist_to_cent
+        if self.precompute_X_norm_squared:
+            self.X_norm_squared_ = (vectors_indexed ** 2).sum(axis=1)
+        else:
+            self.X_norm_squared_ = None
+
+        return self
+
+    def transform(self, X: csr_matrix, y=None, **kwargs) -> csr_matrix:
+        """ Transform distance between query and indexed data with DisSimLocal.
+
+        Parameters
+        ----------
+        X : sparse matrix of shape (n_query, n_indexed)
+            Squared Euclidean distances in sorted sparse neighbors graph,
+            such as obtained from sklearn.neighbors.kneighbors_graph.
+            Each row i must contain the neighbors of X_i in order of increasing distances,
+            that is, nearest neighbor first.
+        y : ignored
+        vectors : array-like of shape (n_query, n_features)
+            Query objects in vector representation.
+            DisSimLocal is formulated specifically for Euclidean distances, and requires access to the objects
+            in the original vector space. (In contrast to other hubness reduction methods, like mutual proximity
+            or local scaling, that operate on dissimilarity data).
+
+        Returns
+        -------
+        A : sparse matrix of shape (n_query, n_indexed)
+            Hubness-reduced graph where A[i, j] is assigned the weight of edge that connects i to j.
+            The matrix is of CSR format.
+
+        Notes
+        -----
+        Ensure sorting when using custom (approximate) neighbors implementations.
+        DisSimLocal strictly requires squared Euclidean distances, and returns undefined values otherwise.
+        """
+        check_is_fitted(self, ['vectors_indexed_', 'centroids_indexed_', 'dist_to_centroids_indexed_'])
+        vectors_query = kwargs.get("vectors", None)
+        vectors_query: np.ndarray = check_array(vectors_query)  # noqa
+        X_query = check_array(X, accept_sparse=True)  # noqa
+        X_query: csr_matrix = X_query.tocsr()
+
+        n_query, n_indexed = X_query.shape
+        n_neighbors = X_query.indptr[1]
+
+        if n_neighbors == 1:
+            warnings.warn(f'Cannot perform hubness reduction with a single neighbor per query. '
+                          f'Skipping hubness reduction, and returning untransformed distances.')
+            return X_query
+
+        k = self.k
+        if k > n_neighbors:
+            k = n_neighbors
+            warnings.warn(f'Neighborhood parameter k larger than number of provided neighbors in X. Reducing to k={k}.')
+
+        # Calculate local neighborhood centroids for query objects among indexed objects
+        neigh_dist = np.empty_like(X_query.data, shape=(n_query, n_neighbors))
+        for i in range(n_query):
+            ind = X_query.getrow(i).indices
+            neigh_dist[i, :] = euclidean_distances(
+                X=vectors_query[i].reshape(1, -1),
+                Y=self.vectors_indexed_[ind],
+                X_norm_squared=self.X_norm_squared_,
+                squared=True,
+            )
+        neigh_ind = X_query.indices
+        knn = neigh_ind[:, :k]
+        centroids_indexed = self.centroids_indexed_[knn].mean(axis=1)
+
+        X_query = X_query - centroids_indexed
+        X_query **= 2
+        X_query_dist_to_centroids = X_query.sum(axis=1)
+        X_indexed_dist_to_centroids = self.dist_to_centroids_indexed_[neigh_ind]
+
+        hub_reduced_dist = neigh_dist
+        hub_reduced_dist -= X_query_dist_to_centroids[:, np.newaxis]
+        hub_reduced_dist -= X_indexed_dist_to_centroids
+
+        # DisSimLocal can yield negative dissimilarities, which can cause problems with
+        # certain scikit-learn routines (e.g. in metric='precomputed' usages).
+        # We, therefore, shift dissimilarities to non-negative values, if necessary.
+        min_dist = hub_reduced_dist.min(initial=0.)
+        if min_dist < 0.:
+            hub_reduced_dist += (-min_dist)
+
+        # Return Euclidean or squared Euclidean distances?
+        if not self.return_squared:
+            np.sqrt(hub_reduced_dist, out=hub_reduced_dist)
+
+        # Return the sorted hubness reduced kneighbors graph
+        return k_neighbors_graph(hub_reduced_dist, original_X=X_query, sort_distances=True)
 
 
 class DisSimLocal(HubnessReduction):
