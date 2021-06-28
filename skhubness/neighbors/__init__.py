5a3bc7c6d962a50ceaaaba21b8353685b2a0c2c2
Roman Feldbauer <sci@feldbauer.org>
2021-06-28 14:46:26 +0200

    MAINT remove sklearn code, part 1

--- skhubness/neighbors/__init__.py
+++ skhubness/neighbors/__init__.py
@@ -6,33 +6,21 @@ approximate nearest neighbor packages. These are compatible with the
 scikit-learn `KNeighborsTransformer`.
 """
 from ._annoy import AnnoyTransformer, LegacyRandomProjectionTree
-from .ball_tree import BallTree
 from .base import VALID_METRICS, VALID_METRICS_SPARSE
 from .classification import KNeighborsClassifier, RadiusNeighborsClassifier
-from .graph import kneighbors_graph, radius_neighbors_graph
 from ._nmslib import NMSlibTransformer, LegacyHNSW
 from .lsh import FalconnLSH, PuffinnLSH
-from .kd_tree import KDTree
 from ._ngt import NGTTransformer, LegacyNNG
-from .dist_metrics import DistanceMetric
 from .regression import KNeighborsRegressor, RadiusNeighborsRegressor
-from .nearest_centroid import NearestCentroid
-from .kde import KernelDensity
-from .lof import LocalOutlierFactor
-from .nca import NeighborhoodComponentsAnalysis
 from .unsupervised import NearestNeighbors
 
 
 __all__ = [
     "AnnoyTransformer",
-    "BallTree",
-    "DistanceMetric",
     "FalconnLSH",
-    "KDTree",
     "LegacyHNSW",
     "KNeighborsClassifier",
     "KNeighborsRegressor",
-    "NearestCentroid",
     "NearestNeighbors",
     "NGTTransformer",
     "NMSlibTransformer",
@@ -40,11 +28,6 @@ __all__ = [
     "LegacyNNG",
     "RadiusNeighborsClassifier",
     "RadiusNeighborsRegressor",
-    "kneighbors_graph",
-    "radius_neighbors_graph",
-    "KernelDensity",
-    "LocalOutlierFactor",
-    "NeighborhoodComponentsAnalysis",
     "VALID_METRICS",
     "VALID_METRICS_SPARSE",
 ]
--- skhubness/neighbors/dist_metrics.py
+++
@@ -1,5 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-
-from sklearn.neighbors.dist_metrics import DistanceMetric
-
-__all__ = ['DistanceMetric']
